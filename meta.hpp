9ed0b5a642787f6876f777f8fedb519c7e74928e
Zinjkov.su <zinjkov.su@gmail.com>
2018-04-05 17:24:19 +1000

    add delete operator=

--- meta.hpp
+++ meta.hpp
@@ -12,6 +12,10 @@ struct meta_traits{
     const uint8_t packet_id = packet_id_;
     const uint8_t payload_size = payload_size_;
     const uint16_t packet_size = packet_size_;
+
+    void operator=(const meta_traits &rv) {
+
+    }
 };
 
 
