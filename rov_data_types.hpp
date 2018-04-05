2832cd4c852c4528396d09b69c69c43ced1b7d0b
Zinjkov.su <zinjkov.su@gmail.com>
2018-04-05 13:26:09 +1000

    rewrite packet structure, meta etc

--- rov_data_types.hpp
+++ rov_data_types.hpp
@@ -6,56 +6,42 @@
 #define RC_ROV_SERVER_ROV_DATA_TYPES_HPP
 
 #include <cstdint>
-#include "serializable.hpp"
+#include "base_packet_t.hpp"
+
 
 namespace rov_types {
-    struct rov_telimetry : serializable {
-        enum meta : uint8_t {
-            packet_id = 0x1E,
-            payload_size = 13,
-            packet_size = 15
-        };
+    struct rov_telimetry : public base_packet_t<0x1E, 13, 15> {
 
         float yaw = 0;
         float pitch = 0;
         float roll = 0;
+    private:
+        void data_serialize(binary_stream &bs) override final;
 
-        std::vector<std::uint8_t> serialize() override final;
-
-        error_code deserialize(const std::vector<std::uint8_t> &input) override final;
+        void data_deserialize(binary_stream &bs) override final;
     };
 
-    struct rov_control : serializable {
-        enum meta: uint8_t{
-            packet_id = 0x2E,
-            payload_size = 5,
-            packet_size = 7
-        };
+    struct rov_control : public base_packet_t<0x2E, 5, 7> {
 
         std::int8_t axis_x = 0;
         std::int8_t axis_y = 0;
         std::int8_t axis_z = 0;
         std::int8_t axis_w = 0;
+    private:
+        void data_serialize(binary_stream &bs) override final;
 
-        virtual std::vector<std::uint8_t> serialize() override final;
-
-        virtual error_code deserialize(const std::vector<std::uint8_t> &input) override final;
+        void data_deserialize(binary_stream &bs) override final;
     };
 
 
-    struct rov_hardware_control : serializable {
-        enum meta: uint8_t{
-            packet_id = 0x1A,
-            payload_size = 9,
-            packet_size = 11
-        };
+    struct rov_hardware_control : public base_packet_t<0x1A, 9, 11> {
 
         std::int8_t horizontal_power[4];
         std::int8_t vertical_power[4];
+    private:
+        void data_serialize(binary_stream &bs) override final;
 
-        virtual std::vector<std::uint8_t> serialize() override final;
-
-        virtual error_code deserialize(const std::vector<std::uint8_t> &input) override final;
+        void data_deserialize(binary_stream &bs) override final;
     };
 };
 
